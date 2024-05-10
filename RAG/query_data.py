fdd75ac25c3df636033d068357c9ca60bbc68a07
nthangavelu7 <nthangavelu7@gatech.edu>
2024-05-10 16:08:41 -0700

    latest code

--- RAG/query_data.py
+++ RAG/query_data.py
@@ -1,5 +1,6 @@
 import argparse
-from langchain.vectorstores.chroma import Chroma
+#from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 
